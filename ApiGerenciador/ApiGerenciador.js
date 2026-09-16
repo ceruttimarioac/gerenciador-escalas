a20f09fe394455dfb2b41cbf5be3fe95f3aa3690
Diogo Tizoni <diogotizoni3@gmail.com>
2026-09-16 20:33:19 -0300

    ajuste apigerenciador

--- ApiGerenciador/ApiGerenciador.js
+++ ApiGerenciador/ApiGerenciador.js
@@ -1,6 +1,6 @@
 import fastify from 'fastify'
 import cors from '@fastify/cors'
-import { ADDRESS_WEB, ADDRESS_API, PORT_API } from './config-apigerenciador.js'
+import { ADDRESS_WEB, ADDRESS_API, PORT_API, KEY_FIREBASE_API } from './config-apigerenciador.js'
 
 const app = fastify({ logger: false });
 
@@ -9,8 +9,45 @@ await app.register(cors, {
 })
 
 
-app.post('/login', async (request) => {
+app.post('/login', async (request, reply) => {
+  try {
+    const { email, password } = request.body;
 
+    const response = await fetch(
+      `https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${KEY_FIREBASE_API}`,
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          email: email,
+          password: password,
+          returnSecureToken: true
+        })
+      }
+    );
+
+    const data = await response.json();
+
+    if (!response.ok) {
+      return {
+        success: false,
+        message: data.error?.message
+      };
+    }
+
+    return {
+      success: true,
+      token: data.idToken
+    };
+
+  } catch (error) {
+    return {
+      success: false,
+      message: error.message
+    };
+  }
 });
 
 app.post('/registrar', async () => ({
